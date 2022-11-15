6957efb886de3525ea22c20298ae7fffa81add92
Tony Mai <tonay6@gmail.com>
2022-11-14 20:39:11 -0600

    save-button-prog

--- Develop/script.js
+++ Develop/script.js
@@ -2,56 +2,32 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(document).ready(function () {
-
+  
   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-  // TODO: Add a listener for click events on the save button. This code should
+
+
   $(".saveBtn").on("click", function () {
     //get nearby values.
     console.log(this);
-    var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
-    var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
+    var text = $(this).siblings(".description").val(); 
+    var time = $(this).parent().attr("id"); 
 
     //set items in local storage.
     localStorage.setItem(time, text);
-})
-  
-$("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
     function hourTracker() {
-        //get current number of hours.
-        var currentHour = moment().hour(); // use of moment.js
-
-        // loop over time blocks
-        $(".time-block").each(function () {
-            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-            console.log( blockHour, currentHour)
-
-            //check if we've moved past this time, click into css/html given classes of past, present, or future
-            if (blockHour < currentHour) {
-                $(this).addClass("past");
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-            }
-            else if (blockHour === currentHour) {
-                $(this).removeClass("past");
-                $(this).addClass("present");
-                $(this).removeClass("future");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
-            }
-        })
-    }
-    hourTracker(); //re-run function
+      //get current number of hours.
+      var currentHour = moment().hour(); // use of moment.js
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+  //
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  //
+  // TODO: Add code to display the current date in the header of the page.
+  hourTracker(); //re-run function
 })
